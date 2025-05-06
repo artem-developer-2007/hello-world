ed35bdccff8368d29cf6cee6521fe586b70056e4
Artem <artm.khokhlov.2007@gmail.com>
2025-05-06 12:05:40 +0300

    hello

--- main.js
+++ main.js
@@ -7,3 +7,22 @@ app.get('/', (request, response) => {
 })
 
 app.listen(3000)
+
+
+let nameOne = 'Артем'
+let nameTwo = 'Артем'
+
+if (nameOne[0] == nameTwo[0]) {
+    console.log('Первые буквы совпадают!!!')
+} else {
+    console.log('Первые буквы не совпадают(((')
+}
+
+
+const factorialChislo = int(input('Введите число, факториал которог нужно посчитать: '))
+
+const proizvedenie = 1
+
+for(i = 1; i < factorialChislo; i++) {
+    proizvedenie = proizvedenie * i111
+}
